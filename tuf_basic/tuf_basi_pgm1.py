7f2154e5bc0cd5a58f81a285d6f6b903fccaad45
Muhammad shafi <shafipu.20@gmail.com>
2026-01-05 10:40:36 +0530

    commited

---
+++ tuf_basic/tuf_basi_pgm1.py
@@ -0,0 +1,14 @@
+"""
+Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+"""
+class Solution:
+    def printNumber(self,n):
+        n=int(n)
+        print(n)
+
+
+Solution_instance= Solution()
+Solution_instance.printNumber(5)
+
